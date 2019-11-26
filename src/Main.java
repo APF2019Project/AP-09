02d04e45f0f5c0c94dcc72359d278040a38cf3ee
alihashemabadi <ali_hashem_abadi@yahoo.com>
2019-11-26 14:56:51 +0330

    master

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("WHAT THE FUCK");
         System.out.println("WTH");
-        System.out.println("Revallllleeeee");
     }
 }
